bbe4101a8760d6ab02c07430d1b1b5f4afa5e80e
Altynbek <ANurgaziyev@beeline.kz>
2020-03-04 16:40:32 +0600

    https://codeforces.com/gym/100135/attachments

---
+++ src/Template.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Template {
+
+    private void solve() {
+        
+    }
+
+    private void run() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+        solve();
+        out.close();
+    }
+
+    public static void main(String[] args) {
+        new Template().run();
+    }
+
+    private BufferedReader br;
+    private StringTokenizer st;
+    private PrintWriter out;
+
+    private String next() {
+        while (st == null || !st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return st.nextToken();
+    }
+
+    private Integer nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    private Long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    private Double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
